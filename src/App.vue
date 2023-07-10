3632ad135340be2874ec25e0f1c49d88bdf949fd
Maxim Misyuchenko <haron9373@gmail.com>
2023-07-10 22:51:55 +0300

    remove links

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,6 @@
   <v-app>
     <v-app-bar :elevation="12" rounded hide density="compact">
       <v-btn variant="tonal" @click="pushToHome">Home</v-btn>
-      <router-link class="app__link" to="buttons">Buttons</router-link>
-      <router-link class="app__link" to="cards">Cards</router-link>
-      <router-link class="app__link" to="chips">Chips</router-link>
-      <router-link class="app__link" to="dialogs">Dialogs</router-link>
-      <router-link class="app__link" to="expansion-panel">Expansion-panel</router-link>
-      <router-link class="app__link" to="list">List</router-link>
-      <router-link class="app__link" to="menus">Menus</router-link>
-      <router-link class="app__link" to="overlays">Overlays</router-link>
-      <router-link class="app__link" to="sheets">Sheets</router-link>
-      <router-link class="app__link" to="toolbars">Toolbars</router-link>
-      <router-link class="app__link" to="tooltips">Tooltips</router-link>
     </v-app-bar>
     <div class="app">
       <router-view />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>Links</h1>
-    <router-link to="buttons">Buttons</router-link>
-    <router-link to="cards">Cards</router-link>
+  <h1>Links</h1>
+  <router-link to="buttons">Buttons</router-link>
+  <router-link to="cards">Cards</router-link>
+  <router-link class="app__link" to="buttons">Buttons</router-link>
+  <router-link class="app__link" to="cards">Cards</router-link>
+  <router-link class="app__link" to="chips">Chips</router-link>
+  <router-link class="app__link" to="dialogs">Dialogs</router-link>
+  <router-link class="app__link" to="expansion-panel">Expansion-panel</router-link>
+  <router-link class="app__link" to="list">List</router-link>
+  <router-link class="app__link" to="menus">Menus</router-link>
+  <router-link class="app__link" to="overlays">Overlays</router-link>
+  <router-link class="app__link" to="sheets">Sheets</router-link>
+  <router-link class="app__link" to="toolbars">Toolbars</router-link>
+  <router-link class="app__link" to="tooltips">Tooltips</router-link>F
 </template>
